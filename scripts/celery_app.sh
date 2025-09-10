84f81262b695d9c677736ebc59a05bc401149ac9
faizuet <faizuet7@gmail.com>
2025-09-10 14:03:54 +0500

    Save current work for comparison

--- scripts/celery_app.sh
+++ scripts/celery_app.sh
@@ -3,9 +3,5 @@ set -e
 
 echo "Starting Celery worker..."
 
-LOG_LEVEL=${LOG_LEVEL:-info}
-
-CONCURRENCY=${CONCURRENCY:-1}
-
-celery -A car_tasks.celery_app worker --loglevel=$LOG_LEVEL --concurrency=$CONCURRENCY
+celery -A car_tasks.celery_app worker --loglevel=info --concurrency=1
 
